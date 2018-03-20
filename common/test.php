770a30d4a11c80581233faa447b22e5982b398c0
Patrick van Mourik <vandemou@gmail.com>
2018-03-20 16:17:24 +0100

    added php + database

--- common/test.php
+++ common/test.php
@@ -1,12 +1,27 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>My first PHP page</h1>
-
 <?php
-echo "Hello World!";
-?>
 
-</body>
-</html>
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "test";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT personID, firstName, lastName FROM tableName";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["personID"]. " - Name: " . $row["firstName"]. " " . $row["lastName"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
